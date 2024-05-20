2ceba232d6bc8cb5b1575b45c33401fdb0cd0646
blackcoffee6681 <100376218+marcos-kennedy@users.noreply.github.com>
2024-05-20 13:31:48 -0300

    coloquei os arquivos nesta página

---
+++ questions.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas auctor nisi eget urna varius, non facilisis massa blandit. Duis dictum orci non massa lobortis rhoncus. Nunc feugiat lorem sit amet dui condimentum laoreet. Duis varius dui sed velit faucibus maximus. Pellentesque lacinia feugiat leo, vitae lacinia nisi sodales ac. Donec nec vulputate nisl. Nulla convallis vulputate risus, quis suscipit lorem tincidunt fermentum. Mauris tempus volutpat nibh, ac eleifend nibh feugiat ut. Mauris in sapien eu nibh consectetur rhoncus.",
+      answers: [
+        { option: "3", correct: true },
+        { option: "70", correct: false },
+        { option: "2", correct: false },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque id diam enim. Etiam porta diam tellus, non pulvinar sapien scelerisque vel. Ut eget tristique lorem. Sed consequat tempor urna ut porta. Integer cursus, orci vel efficitur dictum, est sem mattis orci, ac sodales erat sem vel ligula. Morbi tincidunt tristique vestibulum. Vivamus eu neque luctus, lacinia libero ac, euismod nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+      answers: [
+        { option: "9", correct: false },
+        { option: "70", correct: false },
+        { option: "10", correct: true },
+        { option: "11", correct: false },
+      ],
+    },
+    {
+      question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      answers: [
+        { option: "800", correct: false },
+        { option: "70", correct: false },
+        { option: "90", correct: false },
+        { option: "80", correct: true },
+      ],
+    },
+    {
+      question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      answers: [
+        { option: "70", correct: false },
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+      ],
+    },
+    {
+        question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        answers: [
+          { option: "70", correct: false },
+          { option: "70", correct: false },
+          { option: "56", correct: false },
+          { option: "49", correct: true },
+        ],
+      },
+      {
+        question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        answers: [
+          { option: "70", correct: false },
+          { option: "70", correct: false },
+          { option: "56", correct: false },
+          { option: "49", correct: true },
+        ],
+      },
+      {
+        question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        answers: [
+          { option: "70", correct: false },
+          { option: "70", correct: false },
+          { option: "56", correct: false },
+          { option: "49", correct: true },
+        ],
+      },
+      {
+        question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        answers: [
+          { option: "70", correct: false },
+          { option: "70", correct: false },
+          { option: "56", correct: false },
+          { option: "49", correct: true },
+        ],
+      },
+      {
+        question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        answers: [
+          { option: "70", correct: false },
+          { option: "70", correct: false },
+          { option: "56", correct: false },
+          { option: "49", correct: true },
+        ],
+      },
+      {
+        question: "Donec scelerisque sed risus eu posuere. Ut a dictum leo. Vivamus vestibulum elit non diam lacinia, rutrum consectetur leo ultrices. Suspendisse fringilla nunc libero, vitae rhoncus diam faucibus et. Pellentesque sit amet elit gravida, viverra velit vel, lobortis lorem. Nullam tincidunt molestie libero, in tristique nisi pretium id. Suspendisse tincidunt sed nisl vitae scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        answers: [
+          { option: "70", correct: false },
+          { option: "70", correct: false },
+          { option: "56", correct: false },
+          { option: "49", correct: true },
+        ],
+      },
+  ];
